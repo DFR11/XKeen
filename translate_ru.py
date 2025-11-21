a9d052e31d64c67c1b1567d48341ef0e59a8b1ac
DFR11 <82621304+DFR11@users.noreply.github.com>
2025-11-21 11:49:22 +0800

    Update translate_ru.py

--- translate_ru.py
+++ translate_ru.py
@@ -142,7 +142,7 @@ def main():
     exclude_dirs = ['.git', '.github']
     
     # --- 关键修改：在这里添加 'config' 这种无后缀文件 ---
-    target_files = ['config', 'Makefile', 'xkeen', 'OffLine_install.md', 'FileDescriptors.md'] 
+    target_files = ['config', 'Makefile', 'xkeen', 'OffLine_install', 'FileDescriptors'] 
     target_exts = ['.sh', '.cfg', '.conf', '.list', '.lst', '.md'] 
 
     for root, dirs, files in os.walk("."):
